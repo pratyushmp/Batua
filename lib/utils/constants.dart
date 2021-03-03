8f349cffc59f883c1c127db0c9b92debcb437068
Pratyush M <mpratyush2008@gmail.com>
2021-03-03 11:59:30 +0530

    Added constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primary_color = Color(0xFFf5f6fb);
+Color secondary_color = Color(0xFF4731ee);
+Color text_accent_primary = Color(0xFF8E94AD);
+Color text_accent_secondary = Color(0xFFDBDEEB);
